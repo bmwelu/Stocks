abf77564f0f646e2a5df7982c5008f8ffafaac1f
Brandon Welu <bmwelu@gmail.com>
2018-06-11 20:59:37 -0500

    fix openshift server starting bugs

--- StockScreener/server/index.js
+++ StockScreener/server/index.js
@@ -2,13 +2,6 @@ var express = require('express');
 var stocks = require('./routes/stocks');
 var sectors = require('./routes/sectors');
 var app = express();
-const DefaultPort = 80;
-
-var port = process.env.PORT || DefaultPort;
-
-var detectDebug = function() {
-    return process.env.NODE_ENV !== 'production';
-};
 
 // Add headers
 app.use(function (req, res, next) {
@@ -21,18 +14,9 @@ app.use('/', stocks);
 app.use('/stocks', stocks);
 app.use('/sectors', sectors);
 
-if (detectDebug)
-{
-    app.listen(port, function(){
-        console.log('Server started on port ' + port);
-    });
-}
-else //Openshift deploy
-{
-    var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-    var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 80
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || 'localhost'
 
-    server.listen(server_port, server_ip_address, function () {
-        console.log( "Listening on " + server_ip_address + ", server_port " + port )
-    });
-}
+app.listen(server_port, server_ip_address, function () {
+    console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
+});
