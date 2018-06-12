5a80e7231f62922b42ac716ca965eecbd87b18c9
Brandon Welu <bmwelu@gmail.com>
2018-06-11 20:45:17 -0500

    Update server app start for OpenShift

--- StockScreener/server/index.js
+++ StockScreener/server/index.js
@@ -6,6 +6,10 @@ const DefaultPort = 80;
 
 var port = process.env.PORT || DefaultPort;
 
+var detectDebug = function() {
+    return process.env.NODE_ENV !== 'production';
+};
+
 // Add headers
 app.use(function (req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -17,6 +21,18 @@ app.use('/', stocks);
 app.use('/stocks', stocks);
 app.use('/sectors', sectors);
 
-app.listen(port, function(){
-	console.log('Server started on port ' + port);
-});
+if (detectDebug)
+{
+    app.listen(port, function(){
+        console.log('Server started on port ' + port);
+    });
+}
+else //Openshift deploy
+{
+    var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+    var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+    server.listen(server_port, server_ip_address, function () {
+        console.log( "Listening on " + server_ip_address + ", server_port " + port )
+    });
+}
