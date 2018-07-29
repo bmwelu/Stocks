fb842227506856ae0302a7f3f830efc88a20fea6
Brandon Welu <bmwelu@gmail.com>
2018-07-29 17:57:52 -0500

    update to allows CORS

--- StockScreener/server/index.js
+++ StockScreener/server/index.js
@@ -8,7 +8,11 @@ const DefaultPort = 8080;
 var port = process.env.PORT || DefaultPort;
 
 // Add headers
-app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.use('/', stocks);
 app.use('/stocks', stocks);
