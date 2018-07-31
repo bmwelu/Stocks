2d1f2cc05e167409242adb8a2ecef152137edd76
Brandon Welu <bmwelu@gmail.com>
2018-07-30 20:13:31 -0500

    Update to host server code on azure

--- StockScreener/client/src/environments/environment.prod.ts
+++ StockScreener/client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiURL: 'https://stock-server-stock-server.193b.starter-ca-central-1.openshiftapps.com/'
+  apiURL: 'https://bwelu-server.azurewebsites.net/'
 };
